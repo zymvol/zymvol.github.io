7df76a0826d3432527b8639484365c196a4b746f
vgil <victor.gil.sepulveda@gmail.com>
2017-04-10 13:15:37 +0200

    new version without scrollify

--- js/agency.js
+++ js/agency.js
@@ -5,48 +5,40 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+$(document).ready(function(){
+	main();
+});
+
+function main(){
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+	var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1500, 'easeInOutCubic');
         event.preventDefault();
     });
 
-    $.scrollify({
-        section : ".scrollyfiable",
-        interstitialSection : ".scrollifiable-mini",
-        setHeights: false,
-	touchScroll: true
-    });
-});
+		// Highlight the top nav as scrolling occurs
+		$('body').scrollspy({
+		    target: '.navbar-fixed-top'
+		})
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+		// Closes the Responsive Menu on Menu Item Click
+		$('.navbar-collapse ul li a').click(function() {
+		    $('.navbar-toggle:visible').click();
+		});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
-});
-
-$(window).scroll(function(){
-    if ( $(window).scrollTop() > 120 ) {
-      $('.logo').addClass('scrolling');
-    } else {
-      $('.logo').removeClass('scrolling');
-  }
-});
+	
+	// logo changes when we go outside the header
+	// TODO: use scrollspy?
+	$(window).scroll(function(){
+	    if ( $(window).scrollTop() > 120 ) {
+	      $('.logo').addClass('scrolling');
+	    } 
+	    else {
+	      $('.logo').removeClass('scrolling');
+	    }
+	});
+	
+	console.log($("nav").outerHeight());
+}
